e05e8529382d2730dc6292562f7d3c2e57101163
Steven <Steven.c.hunt@gmail.com>
2018-07-24 14:39:41 -0700

    Testing database for TDD

---
+++ TDD/test_connection.php
@@ -0,0 +1,9 @@
+<?php
+    // Database: AWS RDS Credentials
+    $host                   = 'cst438.xxxxxxxxxxx.us-west-2.rds.amazonaws.com:3306';
+    $port                   = '3306';
+    $dbname                 = 'test_db'; 
+    $username               = 'xxxxxxx'; 
+    $password               = 'xxxxxxx';
+    $conn = mysqli_connect($host, $username, $password); 
+?>
